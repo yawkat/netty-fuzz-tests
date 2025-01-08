4971c98dd4d7714c48da3907e6bdab09aea3315f
yawkat <jonas.konrad@oracle.com>
2025-01-08 18:55:05 +0100

    more fixes

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-apt-get install autoconf automake libtool make tar gcc
+apt-get install -y autoconf automake libtool make tar gcc
 
 pushd "netty"
   ./mvnw install -DskipTests -Dcheckstyle.skip
 popd
 
-export OSSFUZZ_NETTY_VERSION=$(cd micronaut-core && mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
+export OSSFUZZ_NETTY_VERSION=$(cd netty && ./mvnw org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
 cd netty-fuzz-tests
 
