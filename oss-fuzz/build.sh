a4f471af494ae2601f549341264530cb63dc991c
yawkat <jonas.konrad@oracle.com>
2025-01-08 18:19:48 +0100

    use mvnw

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 pushd "netty"
-  mvn install -DskipTests -Dcheckstyle.skip
+  ./mvnw install -DskipTests -Dcheckstyle.skip
 popd
 
 export OSSFUZZ_NETTY_VERSION=$(cd micronaut-core && mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
